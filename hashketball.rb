18e51fc969aff2b3bf5d4ec44fc2a930976778d6
Nathan Alex <nate745@gmail.com>
2019-10-06 02:40:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -134,3 +134,152 @@ def num_points_scored(player)
     end
   end
 end
+
+def shoe_size(player)
+  game_hash.reduce({}) do |memo, (key, value)|
+    a = game_hash[key]
+    b = a[:players]
+    b.each_pair do |key, value|
+      if key == player
+        b[key].each_pair do |key, value|
+          return value if key == :shoe
+        end
+      end
+    end
+  end
+end
+
+
+def team_colors(team)
+  game_hash.reduce({}) do |memo, (key, value)|
+    a = game_hash[key]
+    color = a[:colors]
+    name =  a[:team_name]
+    return color if name == team
+  end
+end
+
+
+def team_names
+  names_array = []
+  game_hash.reduce({}) do |memo, (key, value)|
+    a = game_hash[key]
+    color = a[:colors]
+    names_array << a[:team_name]
+  end
+  return names_array
+end
+
+
+def player_numbers(team)
+  jersey_numbers = []
+  game_hash.reduce({}) do |memo, (key, value)|
+    a = game_hash[key]
+    game_hash[key].reduce({}) do |memo, (key, value)|
+    if a[:team_name] == team
+      a[:players].reduce({}) do |memo, (key, value)|
+        jersey_numbers << a[:players][key][:number]
+      end
+     end 
+    end
+  end
+  return jersey_numbers.uniq
+end
+
+
+def player_stats(player)
+  game_hash.reduce({}) do |memo, (key, value)|
+    a = game_hash[key]
+    a[:players].reduce({}) do |memo, (key, value)|
+      return value if key == player
+    end
+  end
+end
+
+
+def big_shoe_rebounds
+  big_shoe = 0 
+  rebounds = 0
+  game_hash.reduce({}) do |memo, (key, value)|
+    a = game_hash[key]
+    a[:players].reduce({}) do |memo, (key, value)|
+    if a[:players][key][:shoe] > big_shoe
+      big_shoe = a[:players][key][:shoe]
+      rebounds = a[:players][key][:rebounds]
+    end
+    end
+  end
+  return rebounds
+end
+
+def most_points_scored
+  most_points = 0 
+  most_points_name = ""
+  game_hash.reduce({}) do |memo, (key, value)|
+    a = game_hash[key]
+    a[:players].reduce({}) do |memo, (key, value)|
+    name = key
+    if a[:players][key][:points] > most_points
+      most_points = a[:players][key][:points]
+      most_points_name = name
+    end
+    end
+  end
+  return most_points_name
+end
+
+def winning_team
+  winner_points = 0 
+  winner_name = ""
+  game_hash.reduce({}) do |memo, (key, value)|
+    a = game_hash[key]
+    current_points = 0
+    name = a[:team_name]
+    a[:players].reduce({}) do |memo, (key, value)|
+      current_points += a[:players][key][:points]
+    end
+    if current_points > winner_points
+      winner_points = current_points
+      winner_name = name
+    end
+  end
+  return winner_name
+end
+
+def player_with_longest_name
+  longest_name_length = 0 
+  longest_name = ""
+  game_hash.reduce({}) do |memo, (key, value)|
+    a = game_hash[key]  
+    a[:players].reduce({}) do |memo, (key, value)|
+      if key.length > longest_name_length
+        longest_name_length = key.length
+        longest_name = key
+      end
+    end
+  end
+  return longest_name
+end
+
+def long_name_steals_a_ton?
+  longest_name = player_with_longest_name
+  most_rebounds = most_rebounds_scored
+  p longest_name == most_rebounds
+
+end
+
+def most_rebounds_scored
+  most_points = 0 
+  most_points_name = ""
+  game_hash.reduce({}) do |memo, (key, value)|
+    a = game_hash[key]
+    a[:players].reduce({}) do |memo, (key, value)|
+    name = key
+    if a[:players][key][:steals] > most_points
+      most_points = a[:players][key][:steals]
+      most_points_name = name
+    end
+    end
+  end
+  return most_points_name
+end 
