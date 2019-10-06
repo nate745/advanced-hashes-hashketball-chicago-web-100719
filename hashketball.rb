dc1d90a6e4d178d4d96e08916ece297553141878
Nathan Alex <nate745@gmail.com>
2019-10-06 01:45:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,57 +5,60 @@ def game_hash
       :colors => ["Black", "White"],
       :players => {
         "Alan Anderson" => {
-         :number => 0,
-         :shoe => 16,
-         :points => 22,
-         :rebounds => 12,
-         :assists => 12,
-         :steals => 3,
-         :blocks => 1,
-         :slam_dunks => 1
-      },
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
+        },
         "Reggie Evans" => {
-          :number => 30
-          :shoe => 14
-          :points => 12
-          :rebounds => 12
-          :assists => 12
-          :steals => 12
-          :blocks => 12
-          :slam_dunks => 7
+          :number => 30,
+          :shoe => 14,
+          :points => 12,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 12,
+          :blocks => 12,
+          :slam_dunks => 7          
         },
         "Brook Lopez" => {
-          :number => 11
-          :shoe => 17
-          :points => 17
-          :rebounds => 19
-          :assists => 10
-          :steals => 3
-          :blocks => 1
-          :slam_dunks => 15
+          :number => 11,
+          :shoe => 17,
+          :points => 17,
+          :rebounds => 19,
+          :assists => 10,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 15          
         },
         "Mason Plumlee" => {
-          :number => 1
-          :shoe => 19
-          :points => 26
-          :rebounds => 11
-          :assists => 6
-          :steals => 3
-          :blocks => 8
-          :slam_dunks => 5
+          :number => 1,
+          :shoe => 19,
+          :points => 26,
+          :rebounds => 11,
+          :assists => 6,
+          :steals => 3,
+          :blocks => 8,
+          :slam_dunks => 5           
         },
         "Jason Terry" => {
-          :number => 31
-          :shoe => 15
-          :points => 19
-          :rebounds => 2
-          :assists => 2
-          :steals => 4
-          :blocks => 11
-          :slam_dunks => 1
+          :number => 31,
+          :shoe => 15,
+          :points => 19,
+          :rebounds => 2,
+          :assists => 2,
+          :steals => 4,
+          :blocks => 11,
+          :slam_dunks => 1           
         }
+
+
       }
     },
+
 
     :away => {
       :team_name => "Charlotte Hornets",
@@ -69,7 +72,7 @@ def game_hash
           :assists => 1,
           :steals => 2,
           :blocks => 7,
-          :slam_dunks => 2
+          :slam_dunks => 2          
         },
         "Bismack Biyombo" => {
           :number => 0,
@@ -79,7 +82,7 @@ def game_hash
           :assists => 7,
           :steals => 22,
           :blocks => 15,
-          :slam_dunks => 10
+          :slam_dunks => 10           
         },
         "DeSagna Diop" => {
           :number => 2,
@@ -89,7 +92,7 @@ def game_hash
           :assists => 12,
           :steals => 4,
           :blocks => 5,
-          :slam_dunks => 5
+          :slam_dunks => 5           
         },
         "Ben Gordon" => {
           :number => 8,
@@ -99,7 +102,7 @@ def game_hash
           :assists => 2,
           :steals => 1,
           :blocks => 1,
-          :slam_dunks => 0
+          :slam_dunks => 0          
         },
         "Kemba Walker" => {
           :number => 33,
@@ -109,11 +112,11 @@ def game_hash
           :assists => 12,
           :steals => 7,
           :blocks => 5,
-          :slam_dunks => 12
+          :slam_dunks => 12           
         }
 
 
-      }
+      }      
     }
   }
 end
